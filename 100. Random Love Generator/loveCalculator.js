2eaa7b74a5575ed33264f4183baa6b4933c22e62
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2023-11-23 17:08:31 +0200

    initial commit

---
+++ 100. Random Love Generator/loveCalculator.js
@@ -0,0 +1,22 @@
+//input name of the user
+alert("Welcome to love calculator");
+
+let names = document.getElementById("Names")
+
+
+
+names.addEventListener("submit", ()=>{
+    let userName = document.getElementById("user-name");
+    let crushName = document.getElementById("crush-name");
+
+    if(userName==="" || crushName ===""){
+        alert("input some variables");
+    }else{
+        
+    }
+})
+/* a piece of code for random numbers
+var n = Math.random();
+const randomNumber = Math.floor(n * 6) + 1;
+console.log(randomNumber);
+*/
